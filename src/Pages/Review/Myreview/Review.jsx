db1dab7f14a13f5c7617a461398e2b5cd5214e29
coder-sadhin <codersadhin@gmail.com>
2022-11-10 16:39:52 +0600

    remove some exra code and fixed responsive

--- src/Pages/Review/Myreview/Review.jsx
+++ src/Pages/Review/Myreview/Review.jsx
@@ -7,7 +7,7 @@ const Review = ({ review, handleToDeleteComment }) => {
     return (
         <div className='w-full flex md:flex-row flex-col justify-between border border-blue-700 rounded-xl bg-blue-200'>
             <div className="avatar">
-                <div className="sm:w-24 w-60 mx-auto py-5 rounded-xl">
+                <div className="sm:w-24 md:w-24 lg:w-24 w-60 mx-auto py-5 sm:py-0 md:py-0 lg:py-0 rounded-xl">
                     <img className='rounded-lg' src={image_url} alt="" />
                 </div>
             </div>
@@ -15,7 +15,7 @@ const Review = ({ review, handleToDeleteComment }) => {
                 <h2 className='text-center sm:text-2xl font-bold text-purple-500'>Service Name:- {serviceName}</h2>
                 <p className='text-xl text-center text-pink-500'>{massage}</p>
             </div>
-            <div className='flex justify-center items-center'>
+            <div className='flex justify-center py-5 items-center'>
                 <Link className='btn btn-primary mr-5' to={`/updateReview/${_id}`}>Update</Link>
                 <button onClick={() => handleToDeleteComment(_id)} className='btn btn-outline bg-orange-400 mr-5'>X</button>
             </div>
